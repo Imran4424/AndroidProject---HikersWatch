fc3ffa03e05df92f9567fbe7eecce1cd492ab157
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-10-17 16:40:11 +0600

    main activiy

--- app/src/main/java/com/imran/android/hikerswatch/MainActivity.java
+++ app/src/main/java/com/imran/android/hikerswatch/MainActivity.java
@@ -34,7 +34,8 @@ public class MainActivity extends AppCompatActivity {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
         } else {
-            
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+            Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
         }
     }
 }
