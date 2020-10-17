dcfd48dcc9a9d79abfc256a8113a1816a218a9c3
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-10-17 16:12:32 +0600

    main activiy

--- app/src/main/java/com/imran/android/hikerswatch/MainActivity.java
+++ app/src/main/java/com/imran/android/hikerswatch/MainActivity.java
@@ -1,8 +1,10 @@
 package com.imran.android.hikerswatch;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
@@ -17,5 +19,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(@NonNull Location location) {
+                
+            }
+        }
     }
 }
