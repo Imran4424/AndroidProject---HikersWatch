e1dd84d104d67b6a0dfc6a1c1896c36fb8da62d5
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-10-17 21:41:00 +0600

    main activiy

--- app/src/main/java/com/imran/android/hikerswatch/MainActivity.java
+++ app/src/main/java/com/imran/android/hikerswatch/MainActivity.java
@@ -6,13 +6,20 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.widget.TextView;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
     LocationManager locationManager;
@@ -59,13 +66,27 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    @SuppressLint("SetTextI18n")
     public void updateLocationInfo(Location location) {
-        TextView lattitudeTextView = findViewById(R.id.lattitudeText);
+        TextView latitudeTextView = findViewById(R.id.latitudeText);
         TextView longitudeTextView = findViewById(R.id.longitudeText);
         TextView accuracyTextView = findViewById(R.id.accuracyText);
         TextView altitudeTextView = findViewById(R.id.altitudeText);
         TextView addressTextView = findViewById(R.id.addressText);
 
-        
+        latitudeTextView.setText("Latitude: " + Double.toString(location.getLatitude()));
+        longitudeTextView.setText("Latitude: " + Double.toString(location.getLongitude()));
+        accuracyTextView.setText("Latitude: " + Double.toString(location.getAccuracy()));
+        altitudeTextView.setText("Latitude: " + Double.toString(location.getAltitude()));
+
+        String address = "\nCould Not find address :(";
+
+        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+
+        try {
+            List <Address> listAddress = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
