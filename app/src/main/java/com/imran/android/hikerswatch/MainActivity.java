6dafcbf53d85ada689386ba72c804d37383aecff
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-10-17 17:15:17 +0600

    main activiy

--- app/src/main/java/com/imran/android/hikerswatch/MainActivity.java
+++ app/src/main/java/com/imran/android/hikerswatch/MainActivity.java
@@ -50,7 +50,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
         if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
+            
         }
     }
 
